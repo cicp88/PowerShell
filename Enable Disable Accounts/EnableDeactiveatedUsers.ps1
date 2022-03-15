bce5ff5c2f54f97206aa2389d6960c9838f07fd9
cicp88 <christian.c.pinon@outlook.com>
2022-03-15 12:53:04 -0700

    Version 1

---
+++ Enable Disable Accounts/EnableDeactiveatedUsers.ps1
@@ -0,0 +1,6 @@
+#Enable deactivated Users without enabling the guest, defaultaccount, or krbtgt accounts
+#this is assuming the guest, defaultaccount, and krbtgt accounts are in the Users container
+
+Get-ADUser -Filter {(enabled -eq $false)} | 
+ Where-Object {$_.DistinguishedName -notlike "*CN=Users,DC=Adatum,DC=com"} | 
+ Enable-ADAccount
