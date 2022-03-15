f2c7a516cf2293ea70e0c4b735b33a37f45b676d
cicp88 <christian.c.pinon@outlook.com>
2022-03-15 14:54:49 -0700

    Version 1

---
+++ Enable Disable Accounts/DisableInactiveUsers.ps1
@@ -0,0 +1,17 @@
+﻿ #Disable user accounts that have not logged on in over 60 days
+
+ #NOTE: Your service accounts should be moved to an OU to prevent them from being inadvertedly deactivated
+ #Change -TimeSpan value to match corporate security policy
+ 
+ $exclued = 'OU=Service Accounts,DC=Adatum,DC=com' #modify this to match your organization
+    
+ Search-ADAccount -UsersOnly -AccountInactive -TimeSpan 60:00:00:00 |
+     ForEach-Object{
+         if ( ($_.distinguishedName -match '(?=OU=|DC=)(.*\n?)(?<=.)') -and ($exclued -contains $matches[1])){
+             "Excluded $($_.Name)"
+         }
+         else{
+             "Disabling $($_.Name)"
+             $_ | Disable-ADAccount
+         }
+     }
