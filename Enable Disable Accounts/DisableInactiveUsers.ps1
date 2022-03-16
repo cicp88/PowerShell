fd9bbfeae1880efc06e691e5737982a0af339313
cicp88 <christian.c.pinon@outlook.com>
2022-03-16 14:08:15 -0700

    Version 3

--- Enable Disable Accounts/DisableInactiveUsers.ps1
+++ Enable Disable Accounts/DisableInactiveUsers.ps1
@@ -1,17 +1,18 @@
-﻿ #Disable user accounts that have not logged on in over 60 days
+﻿#Disable user accounts that have not logged on in over 60 days
 
- #NOTE: Your service accounts should be moved to an OU to prevent them from being inadvertedly deactivated
- #Change -TimeSpan value to match corporate security policy
- 
- $exclued = 'OU=Service Accounts,DC=Adatum,DC=com' #modify this to match your organization
+#NOTE: Your service accounts should be moved to an OU to prevent them from being inadvertedly deactivated
+#NOTE: Keep in mind persons on long term leave!
+#Change -TimeSpan value to match corporate security policy
     
- Search-ADAccount -UsersOnly -AccountInactive -TimeSpan 60:00:00:00 |
-     ForEach-Object{
-         if ( ($_.distinguishedName -match '(?=OU=|DC=|CN=)(.*\n?)(?<=.)') -and ($exclued -contains $matches[1])){
-             "Excluded $($_.Name)"
-         }
-         else{
-             "Disabling $($_.Name)"
-             $_ | Disable-ADAccount
-         }
-     }
+$exclued = 'OU=Service Accounts,DC=Adatum,DC=com','OU=Long Term Leave,DC=Adatum,DC=com' #modify this to match your organization
+         
+Search-ADAccount -UsersOnly -AccountInactive -TimeSpan 60:00:00:00 |
+    ForEach-Object{
+        if ( ($_.distinguishedName -match '(?=OU=|DC=|CN=)(.*\n?)(?<=.)') -and ($exclued -contains $matches[1])){
+            "Excluded $($_.Name)"
+        }
+        else{
+            "Disabling $($_.Name)"
+            $_ | Disable-ADAccount
+        }
+    }
