e11f319ada05e9ba6c88f92f2b0489acab44f59b
cicp88 <christian.c.pinon@outlook.com>
2022-03-16 10:19:30 -0700

    Version 1

--- Network connectivity troubleshooting/NetIpAddressRemoteHost.ps1
+++ Network connectivity troubleshooting/NetIpAddressRemoteHost.ps1
@@ -1,8 +1,8 @@
-#Restart a remote host
+#Get-NetIpAddress from a remote host
 
 #Instruct the user to click "start" and type "powershell"
 #Have them click on Windows PowerShell and type "hostname"
 
 $ComputerName = "hostname" #replace hostname with actual computer's name
 
-invoke-command -ComputerName $ComputerName -scriptblock {Restart-Computer}
+Invoke-Command -ComputerName $ComputerName -ScriptBlock {Get-NetIpAddress}
