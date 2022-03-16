314d2b6dde5c4cb559a9edc08928d8092684e27b
cicp88 <christian.c.pinon@outlook.com>
2022-03-16 14:50:23 -0700

    Version 1

---
+++ Network connectivity troubleshooting/NetAdapterRemoteHost.ps1
@@ -0,0 +1,8 @@
+#Get-NetAdapter on a remote host
+
+#Instruct the user to click "start" and type "powershell"
+#Have them click on Windows PowerShell and type "hostname"
+
+$ComputerName = "hostname" #replace hostname with actual computer's name
+
+invoke-command -ComputerName $ComputerName -scriptblock {Get-NetAdapter}
